b4b0e3b6414de6d96d923fbc800fcb7777f733af
ryuhei shibata <shibataryuhei>
2019-12-08 15:21:52 +0900

    [finish]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
 
+  get 'todolists/new'
   get 'top' => 'root#top'
   get "/a", to: "aa#aa"
+
+  post 'todolists' => 'todolists#create'
+  get 'todolists' => 'todolists#index'
 
 end
