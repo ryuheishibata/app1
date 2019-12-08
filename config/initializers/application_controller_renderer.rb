9d90615da5aad972fa9de0bad30bedf6ba75ab29
ryuhei shibata <shibataryuhei>
2019-12-08 20:27:01 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2b964c2dccc8ddee482668af5c213c009988e08efda42076feb3c6a03076686ebc0aeea11d7c3a6bc962a1e499850fe14029e737afd4a3bb208c3d1599134567'
